a0c492816afdbd27ccfb8035f40e08bad7b278bb
lanpn <phan.ngoclan58@gmail.com>
2021-10-24 19:46:25 +0700

    Add backbone config

--- polypnet/model/attn.py
+++ polypnet/model/attn.py
@@ -5,9 +5,10 @@ import pytorch_lightning as pl
 
 
 class AdditiveAttnGate(pl.LightningModule):
-    def __init__(self, x_channels: int, g_channels: int):
+    def __init__(self, x_channels: int, g_channels: int, upsample=True):
         super().__init__()
 
+        self.upsample = upsample
         self.conv_g = nn.Conv2d(g_channels, 1, kernel_size=1, groups=1, bias=True)
         self.conv_x = nn.Conv2d(x_channels, 1, kernel_size=1, groups=1, bias=False)
         self.conv_group = nn.Conv2d(1, 1, kernel_size=1, groups=1, bias=True)
@@ -20,7 +21,9 @@ class AdditiveAttnGate(pl.LightningModule):
         :param g: Coarser output signal, tensor of shape B x C x 2H x 2W
         :return: Filtered output signal (B x C x H x W)
         """
-        down_x = F.interpolate(x, scale_factor=0.5, mode="bilinear")  # B x C x 2H x 2W
+        down_x = x
+        if self.upsample:
+            down_x = F.interpolate(x, scale_factor=0.5, mode="bilinear")  # B x C x 2H x 2W
 
         out_g = self.conv_g(g)  # B x C x 2H x 2W
         out_x = self.conv_x(down_x)  # B x C x 2H x 2W
@@ -30,7 +33,8 @@ class AdditiveAttnGate(pl.LightningModule):
         alpha = self.conv_group(alpha)  # B x C x 2H x 2W
         alpha = nn.Sigmoid()(alpha)  # B x C x 2H x 2W
 
-        alpha = F.interpolate(alpha, scale_factor=2, mode="bilinear")  # B x C x H x W
+        if self.upsample:
+            alpha = F.interpolate(alpha, scale_factor=2, mode="bilinear")  # B x C x H x W
 
         out = x * alpha
 
